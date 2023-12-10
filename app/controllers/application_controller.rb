335af57c3019d425656c61934fc32577982d4580
Mahima Sureka <surekamahima96@gmail.com>
2023-12-09 22:08:59 -0600

    Added feedback icon,star rating option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    def logout
+        session.delete(:user_id)
+        redirect_to root_url
+       end
 end
